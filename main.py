c647fc54dff2e3af841dfebf52a4bb3df0b4bcf2
mohammad665 <mohammad.haqqani@gmail.com>
2020-11-14 08:20:35 +1100

    floatlayout

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
 
-
-class AppGrid(Widget):
-    name = ObjectProperty(None)
-    lastname = ObjectProperty(None)
-    email = ObjectProperty(None)
-
-
-
-    def btn_submit(self):
-
-        print(self.name.text, self.email.text, self.lastname.text)
-
-        self.name.text=""
-        self.email.text=""
-        self.lastname.text=""
-        
-
-        
 
 class RateMe(App):
 
     def build(self):
-        return AppGrid()
+        return FloatLayout()
 
 
 if __name__ == "__main__":
