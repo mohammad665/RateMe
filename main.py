cef8e63c41247a69674190ae9b9051ea9426cd61
mohammad665 <mohammad.haqqani@gmail.com>
2020-11-14 08:28:09 +1100

    logging mouse clicks and touch movement

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import kivy
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+class Touch(Widget):
+
+    btn = ObjectProperty(None)
+
+    def on_touch_down(self, touch):
+        print("mouse down", touch)
+        self.btn.opacity = 0.5
+    
+    def on_touch_move(self, touch):
+        print("mouse move", touch)
+    
+    def on_touch_up(self, touch):
+        print("mouse up", touch)
+        self.btn.opacity = 1
+
+
 
 
 class RateMe(App):
 
     def build(self):
-        return FloatLayout()
+        return Touch()
 
 
 if __name__ == "__main__":
