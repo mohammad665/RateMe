a87b088c2c7a92afc1bb2003af7a4cb3478b00a7
mohammad665 <mohammad.haqqani@gmail.com>
2020-11-14 08:56:21 +1100

    multi screen app, if password is equal to Mohammad it navigates to the next window

--- main.py
+++ main.py
@@ -1,41 +1,25 @@
-import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle
-from kivy.graphics import Color
-from kivy.graphics import Line
-
-class Touch(Widget):
-
-    def __init__(self, **kwargs):
-        super(Touch, self).__init__(**kwargs)
-
-        with self.canvas:
-            Line(points=(20,30, 400, 500))
-            Color(1,0,0,0.5, mode="rgba")
-            self.rect = Rectangle(pos=(0,0), size=(50,50))
-            Color(1,1,0,0.5, mode="rgba")
-            self.rect1 = Rectangle(pos=(200,300), size=(50,50))
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-    def on_touch_down(self, touch):
-        self.rect.pos = touch.pos
-        print("mouse down", touch)
-    
-    def on_touch_move(self, touch):
-        self.rect1.pos = touch.pos
-        print("mouse move", touch)
-    
-    def on_touch_up(self, touch):
-        print("mouse up", touch)
+class MainWindow(Screen):
+    pass
+
+class SecondWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
 
 
+kv = Builder.load_file('rateme.kv')
 
 
 class RateMe(App):
 
     def build(self):
-        return Touch()
+        return kv
 
 
 if __name__ == "__main__":
