234339ea9c8848c4634434ca714e4da31713dfbb
mohammad665 <mohammad.haqqani@gmail.com>
2020-11-13 20:30:53 +1100

    same app but with kivy

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
 class AppGrid(Widget):
-   pass
+    name = ObjectProperty(None)
+    lastname = ObjectProperty(None)
+    email = ObjectProperty(None)
+
+
+
+    def btn_submit(self):
+
+        print(self.name.text, self.email.text, self.lastname.text)
+
+        self.name.text=""
+        self.email.text=""
+        self.lastname.text=""
         
 
         
